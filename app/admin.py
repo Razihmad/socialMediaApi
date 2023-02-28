050a2835269b4b4cc07b0397c523007e67098441
Razi Ahmad <mail2razi4u@gmail.com>
2023-02-28 16:19:23 +0530

    Post table created

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(UserInfo)
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ['user','followers','followings']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user','title']
+    
--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
 class UserInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="user")
     followers = models.IntegerField(default=0)
     followings = models.IntegerField(default=0)
-
-
-# class Post(models.Model):
-    # user = models.ForeignKey(User,)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="postBy")
+    title = models.CharField(max_length=1000)
+    description = models.CharField(max_length=100000)
+    createdTime = models.DateTimeField(default=datetime.now())
