94de1eb07afeb4eba8a66c309391eb00848d814b
Razi Ahmad <mail2razi4u@gmail.com>
2023-02-28 16:14:58 +0530

    change in userinfo fields

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="user")
-    followers = models.IntegerField()
-    followings = models.IntegerField()
+    followers = models.IntegerField(default=0)
+    followings = models.IntegerField(default=0)
 
+
+# class Post(models.Model):
+    # user = models.ForeignKey(User,)
